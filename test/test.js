9f9f2f01643e07743b469eedf95ab16bdf02d080
Paolo Fragomeni <hij1nx@me.com>
2011-10-16 00:00:24 -0400

    [test] adding failing test.

--- test/test.js
+++ test/test.js
@@ -5,18 +5,18 @@ var assert = require('assert'),
     vows = require('vows'),
     Plate = require('../lib/plates');
 
-vows.describe('Merge an object literal with one level of depth into a single tag', {
-  'When creating an instance, and saving it': {
-  	topic: function () {
+vows.describe('Division by Zero').addBatch({
+  'when creating a new constructor and providing both data and markup': {
+    topic: function () { 
 
-      var plate = this.plate = new Plates;
-      var html = this.html = '<div id="test"></div>';
-      var data = this.data = { "test": "Hello, World." };
+    	var html = this.html = '<div id="test"></div>';
+    	var data = this.data = { "test": "Hello, World." };
 
-      this.output = plate(html, data);
-    },
-    'it should return the previous instance': function (res) {
-	  assert.strictEqual(this.html, '<div id="test">Hello, World</div>');
+    	return this.output = new Plate(html, data);
+  	},
+
+    'the data is merged into the markup.': function (topic) {
+        assert.equal (topic, '<div id="test">Hello, World.</div>');
     }
   }
-});
+}).export(module);
