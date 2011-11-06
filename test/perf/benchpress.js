12b31c1f117b2d310da57179607d8ddb5b2357ac
Paolo Fragomeni <hij1nx@me.com>
2011-11-06 17:49:33 -0500

    [lib, test] added benchmark goal

---
+++ test/perf/benchpress.js
@@ -0,0 +1,33 @@
+var Benchmark = require('benchmark');
+var suite = new Benchmark.Suite();
+
+var Mustache = require('mustache');
+var Plates = require('../../lib/plates');
+
+suite
+
+  .add('mustache', function() {
+
+	var view = { "foo": "Hello, World" };
+	var template = '<div id="foo">{{foo}}</div><div class="foo">';
+
+	Mustache.to_html(template, view);
+
+  })
+  .add('plates', function() {
+
+  	var template = '<div id="foo"></div><div class="foo">';
+  	var view = { "foo": "Hello, World" }
+
+  	new Plates(template, view).bind();
+
+  })
+
+  .on('cycle', function(event, bench) {
+    console.log(String(bench));
+  })
+  .on('complete', function() {
+    console.log('Fastest is ' + this.filter('fastest').pluck('name'));
+  })
+
+  .run(true);
