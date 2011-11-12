8b92b47106dda2649ac1fd5d573e86f73c9c23fa
martin_sunset <martin@wawrusch.com>
2011-11-11 23:59:57 -0800

    added failing data bind tests

--- test/api-test.js
+++ test/api-test.js
@@ -5,6 +5,7 @@ var vows = require('vows'),
 vows.describe('merge data into markup').addBatch({
   'when creating a new constructor and providing both data and markup': {
     'for simple template': common.createTest('simple'),
-    'for one level template': common.createTest('one-level')
+    'for one level template': common.createTest('one-level'),
+    'for a template using data-bind': common.createTest('data-bind')
   }
 }).export(module);
