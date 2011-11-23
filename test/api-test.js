d38f21911c476361f3a230ffb5b8d15400160266
Joshua Holbrook <josh.holbrook@gmail.com>
2011-11-22 17:02:18 -0800

    [fix] Tests seem to match the API now. One test still fails, may be an actual regression.

--- test/api-test.js
+++ test/api-test.js
@@ -3,9 +3,8 @@ var vows = require('vows'),
     common = require('./fixtures/common');
 
 vows.describe('merge data into markup').addBatch({
-  'when creating a new constructor and providing both data and markup': {
+  'when providing both data and markup': {
     'for simple template': common.createTest('simple'),
     'for one level template': common.createTest('one-level'),
-    'for a template using data-bind': common.createTest('data-bind')
   }
 }).export(module);
