d741159d34402cb8a2dcdde727f658690c0f4df3
Rob Ashton <robashton@codeofrob.com>
2011-12-07 10:58:30 +0100

    Added (missing?) test case for data-bind and a (failing?) test for overriding the destination

--- test/api-test.js
+++ test/api-test.js
@@ -6,5 +6,7 @@ vows.describe('merge data into markup').addBatch({
   'when providing both data and markup': {
     'for simple template': common.createTest('simple'),
     'for one level template': common.createTest('one-level'),
-  }
+    'for a data-bound template': common.createTest('data-bind', { "name": "data-bind" }),
+    'for an overridden destination': common.createTest('attribute', { "url": ["data-bind", "src"]})
+ }
 }).export(module);
