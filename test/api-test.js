62b19679697aea04532b99af2c0a1573642e75e0
Nathan Hunzaker <nate.hunzaker@gmail.com>
2012-02-21 09:45:50 -0500

    Adds further support for nested elements

--- test/api-test.js
+++ test/api-test.js
@@ -164,9 +164,9 @@ vows.describe('merge data into markup').addBatch({
 
     '(12) iterate a collection of over an element with children.': (
 
-      function() {
+        function() {
           return common.createTest('test-12');
-      }()
+        }()
 
     )
   }
