449d643b3a016f0bd1f82b24f0dbd625b02f46f0
Pedro Teixeira <pedro.teixeira@gmail.com>
2012-09-02 19:58:25 +0100

    [fix] match closing tags properly when iterating. fixes #84.

--- test/api-test.js
+++ test/api-test.js
@@ -442,6 +442,7 @@ vows.describe('merge data into markup').addBatch({
         return common.createTest('test-37', map);
       }()
     ),
+
     '(38) spaces in attributes': (
 
       function() {
@@ -452,6 +453,25 @@ vows.describe('merge data into markup').addBatch({
         return common.createTest('test-38', map);
      
       }()
+    ),
+
+    '(39) Nesting arrays with objects with arrays should work': (
+
+
+      function() {
+
+        var map = Plates.Map();
+
+        // map.className('breadcrumb').to('breadcrumb');
+        // map.className('breadcrumb-element').to('breadcrumb-element');
+        // map.className('category-name').to('breadcrumb-name');
+        // map.className('category-name').to('breadcrumb-url').as('href');
+
+        map.className('author').to('author');
+
+        return common.createTest('test-39', map);
+      }()
+
     )
   }
 
